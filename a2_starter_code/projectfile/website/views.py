b02d5d44dfa763a9436e3a84419e1b05b7f0e007
cole-arm <n11859563@qut.edu.au>
2025-10-07 19:43:03 +1000

    View for History
    updated view for booking history

--- a2_starter_code/projectfile/website/views.py
+++ a2_starter_code/projectfile/website/views.py
@@ -1,7 +1,42 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
     return '<h1>Starter code for assignment 3<h1>'
+
+@main_bp.route('/history')
+def history():
+    
+      # Temporary sample data while DB is paused
+    bookings = [
+        {
+            "id": 1,
+            "title": "Acoustic Night",
+            "venue": "QUT Gardens Theatre",
+            "date": "2025-10-20",
+            "tickets": 2,
+            "status": "Confirmed",
+            "image": "event1.jpg"
+        },
+        {
+            "id": 2,
+            "title": "Tech Expo 2025",
+            "venue": "Brisbane Convention Centre",
+            "date": "2025-11-03",
+            "tickets": 1,
+            "status": "Pending",
+            "image": "event2.jpg"
+        },
+        {
+            "id": 3,
+            "title": "Food Fest",
+            "venue": "South Bank Parklands",
+            "date": "2025-08-14",
+            "tickets": 4,
+            "status": "Cancelled",
+            "image": "event3.jpg"
+        }
+    ]
+    return render_template('history.html', bookings=bookings)
