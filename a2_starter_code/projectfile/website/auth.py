edfca0be12fef5dec553d2ddc9d02fc521056b7f
vinnie-everton <paulo.araujo@connect.qut.edu.au>
2025-10-07 23:43:17 +1000

    Update auth.py
    Route for registering users has been included.

--- a2_starter_code/projectfile/website/auth.py
+++ a2_starter_code/projectfile/website/auth.py
@@ -32,3 +32,23 @@ def login():
         else:
             flash(error)
     return render_template('user.html', form=login_form, heading='Login')
+
+
+# Register Route 
+@auth_bp.route('/register', methods=['GET', 'POST'])
+
+def register():
+    register_form = RegisterForm()
+    error = None
+    if register_form.validate_on_submit():
+        user_name = register_form.user_name.data
+        password = register_form.password.data
+        email = register_form.email.data
+        hashed_pwd = generate_password_hash(register_form.password.data)
+        if user_name is None:
+            error = "Insert a username"
+        if password is None:
+            error = "Insert a password"
+        if email is None: 
+            error = "Insert a email"
+    return render_template('user.html', form=register_form, heading='Register')
