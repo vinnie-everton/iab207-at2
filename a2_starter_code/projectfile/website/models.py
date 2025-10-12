33ef427473a830f7472bf347cc8a4d47b108e22b
vinnie-everton <paulo.araujo@connect.qut.edu.au>
2025-10-12 23:15:06 +1000

    Update to models.py
    Changed certain foreign keys to relationships and added up to date datetime function, and imported timezone.

--- a2_starter_code/projectfile/website/models.py
+++ a2_starter_code/projectfile/website/models.py
@@ -1,5 +1,5 @@
 from . import db
-from datetime import datetime
+from datetime import datetime, timezone
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
@@ -9,6 +9,7 @@ class User(db.Model, UserMixin):
     emailid = db.Column(db.String(100), index=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
     comments = db.relationship('Comment', backref='user')
+    orders = db.relationship('Order', backref='user')
 
 class Event(db.Model):
     __tablename__ = 'event'
@@ -17,18 +18,19 @@ class Event(db.Model):
     category = db.Column(db.String(80)) #picklist
     description = db.Column(db.String(250))
     eventdate = db.Column (db.DateTime) #need to be adjusted
-    startime = db.Column (db.DateTime) #need to be adjusted
+    starttime = db.Column (db.DateTime) #need to be adjusted
     endtime = db.Column (db.DateTime) #need to be adjusted
     venue = db.Column(db.String(80))
     image = db.Column(db.String(80))
     numticket = db.Column (db.Integer)
-    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
+    comments = db.relationship('Comment', backref='event')
+    orders = db.relationship('Order', backref='event')
 
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(400))
-    created_at = db.Column(db.DateTime, default=datetime.now())
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     # add the foreign key
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
@@ -38,6 +40,6 @@ class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer) #needs to be adjusted
     quantity = db.Column(db.Integer)
-    date = db.Column (db.DateTime) #need to be adjusted
+    date = db.Column (db.DateTime, default=lambda: datetime.now(timezone.utc)) #need to be adjusted
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
