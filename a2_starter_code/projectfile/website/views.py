41b39d318f5ec56e0770d71cda3014b96534f69c
josephalfin9 <josephalfin9@gmail.com>
2025-10-15 13:24:54 +1100

    updated views with booking event and and create event routes with ensuring user is logged in and also generates order form

--- a2_starter_code/projectfile/website/views.py
+++ a2_starter_code/projectfile/website/views.py
@@ -1,4 +1,11 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, flash
+from flask_login import login_required, current_user
+from .forms import BookingForm
+from .models import Order
+from .import db
+from datetime import datetime
+from .forms import EventForm
+from .models import Event
 
 main_bp = Blueprint('main', __name__)
 
@@ -47,7 +54,102 @@ def user():
     return render_template("user.html")
 @main_bp.route('/event')
 def event():
-    return render_template("event.html")
-@main_bp.route('/create')
+
+@main_bp.route('/event', methods=['GET', 'POST'])
+@login_required
+def event():
+    form = BookingForm()
+    if form.validate_on_submit():
+        new_order = Order(
+            user_id=current_user.id,
+            quantity=form.ticketQty.data,
+            ticket_type=form.ticketType.data
+        )
+        db.session.add(new_order)
+        db.session.commit()
+        flash(f'Booking successful! Your order ID is {new_order.id}', 'success')
+        return redirect(url_for('main.history'))
+    return render_template('event.html', booking_form=form)
+
+@main_bp.route('/history')
+@login_required
+def history():
+    bookings = Order.query.filter_by(user_id=current_user.id).all()
+    return render_template('history.html', bookings=bookings)
+
+@main_bp.route('/create', methods=['GET', 'POST'])
+@login_required
 def create():
-    return render_template("create.html")
+    form = EventForm()
+    if form.validate_on_submit():
+        # Logic for EVENT STATUS
+        if form.eDate.data<datetime.now():
+            status = 'Inactive'           
+        elif form.eTickets.data == 0:
+            status = 'Sold Out'
+        else:
+            status = 'Open'
+        new_event = Event(
+            eventname=form.eName.data,
+            description=form.eDesc.data,
+            category=form.eCategory.data,
+            venue=form.eVenue.data,
+            eventdate=form.eDate.data,
+            starttime=form.eStart.data,
+            endtime=form.eEnd.data,
+            numticket=form.eTickets.data,
+            image=form.eImageUrl.data,
+            owner_id=current_user.id,
+            status=status
+        )
+        db.session.add(new_event)
+        db.session.commit()
+        flash('Event created successfully!', 'success')
+        return redirect(url_for('main.event'))
+    return render_template('create.html', event_form=form)
+
+@main_bp.route('/event/<int:event_id/edit>', methods=['GET', 'POST'])
+@login_required
+def edit_event(event_id):
+    event = Event.query.get_or_404(event_id)
+    if event.owner_id != current_user.id:
+        flash('You do not have permission to edit this event.', 'danger')
+        return redirect(url_for('main.event'))
+    
+    form = EventForm(obj=event)
+    if form.validate_on_submit():
+        event.eventname = form.eName.data
+        event.description = form.eDesc.data
+        event.category = form.eCategory.data
+        event.venue = form.eVenue.data
+        event.eventdate = form.eDate.data
+        event.starttime = form.eStart.data
+        event.endtime = form.eEnd.data
+        event.numticket = form.eTickets.data
+        event.image = form.eImageUrl.data
+        
+        # Logic for EVENT STATUS
+        if event.eventdate < datetime.now():
+            event.status = 'Inactive'
+        elif event.numticket == 0:
+            event.status = 'Sold Out'
+        else:
+            event.status = 'Open'
+        db.session.commit()
+        flash('Event updated successfully!', 'success')
+        return redirect(url_for('main.index'))
+    
+    return render_template('create.html', event_form=form, edit=True)
+
+@main_bp.route('/event/<int:event_id>/cancel', methods=['POST'])
+@login_required
+def cancel_event(event_id):
+    event = Event.query.get_or_404(event_id)
+    if event.owner_id != current_user.id:
+        flash('You do not have permission to cancel this event.', 'danger')
+        return redirect(url_for('main.event'))
+    
+    event.status = 'Cancelled'
+    db.session.commit()
+    flash('Event cancelled successfully!', 'success')
+    return redirect(url_for('main.index'))
