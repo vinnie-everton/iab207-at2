52903a0ee9eebb146c081f71817325650b14773f
vinnie-everton <paulo.araujo@connect.qut.edu.au>
2025-10-28 21:45:37 +1000

    Update models.py
    Added address and contact rows for db. Recommend deleting db and running created_db.py to create a new one.

--- a2_starter_code/projectfile/website/models.py
+++ a2_starter_code/projectfile/website/models.py
@@ -7,6 +7,8 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(100), index=True, unique=True, nullable=False)
     emailid = db.Column(db.String(100), index=True, nullable=False)
+    address = db.Column(db.String(50))
+    contact = db.Column(db.Integer)
     password_hash = db.Column(db.String(255), nullable=False)
     comments = db.relationship('Comment', backref='user')
     orders = db.relationship('Order', backref='user')
